8ef9d526a8c4ee41d0c3d38a9d022134312849e4
Aldair <naodeletaessaconta@hotmail.com>
2021-01-18 01:31:41 -0300

    Criação do projeto
    Subindo primeira versão do sistema.

---
+++ EstacionamentoRift/EstacionamentoRift/Models/ExampleModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.CollectionGeneric;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.Linq;
+
+namespace teste 
+{
+
+    public class pessoa 
+    {
+        /// <summary>
+        /// Descrição propriedade
+        /// </summary>
+        [Display(Name = "NomePropriedade")]
+        public indexer Codigo { get; set; }
+    }
+}
